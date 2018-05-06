d0074fc04520b7b4e22cdfac29f5cc09c6122fcd
Gaurav Handa <gaurav@aroracomfortechs.com>
2018-05-06 15:55:33 +0530

    added new api

--- server/models/artistcourses.js
+++ server/models/artistcourses.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function(Artistcourses) {
+	Artistcourses.getArtistWithServices = function(date, country, cb) {
+		let whereCondition = {};
+		if(date) {
+			whereCondition = {"startfrom" : new Date(date)};
+		}
+		let whereCountry = {};
+		if(country) {
+			whereCountry = {where: {country : country}}
+		}
 
+		Artistcourses.find(
+				{
+					include: [{
+						relation: 'members',
+						scope: whereCountry
+			        }],
+			        where : whereCondition
+		      }, function(err, results){
+		      cb(null, newArray);
+        });
+	}
+	Artistcourses.remoteMethod('getArtistWithCourses', {
+          http: {path: '/getArtistWithCourses', verb: 'get'},
+          accepts: [
+              {arg: 'date', type: 'string'},
+              {arg: 'country', type: 'string'}],
+          returns: {arg: 'data', type: 'json'}
+    });
+
+  	Artistcourses.remoteMethod('getArtistById', {
+          http: {path: '/getArtistById', verb: 'post'},
+          accepts: [
+              {arg: '', type: 'object', http: { source: 'body' }}],
+          returns: {arg: 'data', type: 'json'}
+    });
 };
